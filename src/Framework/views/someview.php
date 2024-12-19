0f28c7e4205b2e0ab616afb03528494ff90425d8
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 11:26:20 +0100

    what i need

--- src/Framework/views/someview.php
+++ src/Framework/views/someview.php
@@ -2,3 +2,9 @@
 <h1>{{$message}}</h1>
 <?php echo "hello world from php" ?>
 <p>[b]Lorem ipsum[/b], dolor sit amet consectetur adipisicing elit. Ipsa distinctio eligendi laudantium praesentium est tempore vel tenetur inventore in architecto? Placeat sit doloremque reprehenderit alias animi aut, qui sed quia!</p>
+
+<ul>
+@foreach($people as $person)
+    <li>{{$person->name}}</li>
+@endforeach
+</ul>
