2edc4585e0670168f9edf6b1c730b41607a367a6
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 12:33:08 +0100

    what i need

--- src/Framework/views/someview.php
+++ src/Framework/views/someview.php
@@ -7,6 +7,7 @@
 [h6]{{$message}}[/h6]
 <?php echo "hello world from php" ?>
 {"sadsad <b>asdsad</b> asdsad"}
+{!!sadsad <b>asdsad</b> asdsad!!}
 <p>[b]Lorem ipsum[/b], dolor sit amet consectetur adipisicing elit. Ipsa distinctio eligendi laudantium praesentium est tempore vel tenetur inventore in architecto? Placeat sit doloremque reprehenderit alias animi aut, qui sed quia!</p>
 
 [list]
