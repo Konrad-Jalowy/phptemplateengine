f992294b3e810845bbe743ae86c5b706c268cd22
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 11:55:40 +0100

    normalize path method

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -2,4 +2,13 @@
 namespace Framework;
 class Router {
 
+    private function normalizePath(string $path): string
+  {
+    $path = trim($path, '/');
+    $path = "/{$path}/";
+    $path = preg_replace('#[/]{2,}#', '/', $path);
+
+    return $path;
+  }
+
 }
