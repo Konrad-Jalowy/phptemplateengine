c8a91efb3bf273a1945394c2de21f676159086fc
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 11:11:13 +0100

    progress

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
-echo "hello from index.php </br>";
+
 require __DIR__ .  "/../vendor/autoload.php";
 
 use Framework\TemplateEngine;
 
-TemplateEngine::test();
+
 $frameworkPath = __DIR__ . "/../src/Framework";
 $tempEn = new TemplateEngine($frameworkPath, $frameworkPath . "/templatepatterns.php");
-$tempEn->testInstance();
-$tempEn->presentPatterns();
+
+$tempEn->renderTemplate("views/someview.php", ["message" => "hello world"]);
