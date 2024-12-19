6437b3b68168fcaeb8e10d6122c280d1e30c4945
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 11:25:44 +0100

    were getting somewhere

--- public/index.php
+++ public/index.php
@@ -4,8 +4,12 @@ require __DIR__ .  "/../vendor/autoload.php";
 
 use Framework\TemplateEngine;
 
+$people = array(
+    (object)["name" => "John Doe"],
+    (object)["name" => "Jane Doe"]
+);
 
 $frameworkPath = __DIR__ . "/../src/Framework";
 $tempEn = new TemplateEngine($frameworkPath, $frameworkPath . "/templatepatterns.php");
 
-$tempEn->renderTemplate("views/someview.php", ["message" => "hello world"]);
+$tempEn->renderTemplate("views/someview.php", ["message" => "hello world", "people" => $people]);
