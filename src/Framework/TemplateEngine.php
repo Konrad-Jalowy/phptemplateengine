d619103b6ae764739c73c946032a449740908f8a
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 10:34:45 +0100

    were getting somewhere

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -1,7 +1,15 @@
 <?php
 namespace Framework;
 class TemplateEngine {
-    public static function test(){
-        echo "hello world from autoloaded class </br>";
-    }
+    
+
+  private array $globalTemplateData = [];
+
+  public function __construct(private string $basePath)
+  {
+  }
+
+  public static function test(){
+    echo "hello world from autoloaded class </br>";
+}
 }
