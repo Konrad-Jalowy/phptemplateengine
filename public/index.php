e579124d05e5bc15b721f3402e461909ac4f6d2c
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 13:20:24 +0100

    were getting somewhere

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ $loggedIn = true;
 $frameworkPath = __DIR__ . "/../src/Framework";
 $tempEn = new TemplateEngine($frameworkPath, $frameworkPath . "/templatepatterns.php");
 $tempEn->addGlobal('site_name', 'MyApp');
+$tempEn->addGlobal('csrftoken', 'sadsadsadqwdqdwqd');
 $tempEn->renderTemplate("views/someview.php", [
     "message" => "hello world", 
     "people" => $people, 
