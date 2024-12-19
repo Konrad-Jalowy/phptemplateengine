e3be126b20eda47a4b1d4e6033b609d135a3786f
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 11:29:02 +0100

    were getting somewhere

--- public/index.php
+++ public/index.php
@@ -8,8 +8,13 @@ $people = array(
     (object)["name" => "John Doe"],
     (object)["name" => "Jane Doe"]
 );
+$loggedIn = true;
 
 $frameworkPath = __DIR__ . "/../src/Framework";
 $tempEn = new TemplateEngine($frameworkPath, $frameworkPath . "/templatepatterns.php");
 
-$tempEn->renderTemplate("views/someview.php", ["message" => "hello world", "people" => $people]);
+$tempEn->renderTemplate("views/someview.php", [
+    "message" => "hello world", 
+    "people" => $people, 
+    "loggedIn" => $loggedIn
+]);
