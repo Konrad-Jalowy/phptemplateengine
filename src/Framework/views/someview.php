1698373147b73bdfb975881e581647e3a30257e8
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-19 11:19:21 +0100

    bbcode works

--- src/Framework/views/someview.php
+++ src/Framework/views/someview.php
@@ -1,4 +1,4 @@
 
 <h1>{{$message}}</h1>
 <?php echo "hello world from php" ?>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa distinctio eligendi laudantium praesentium est tempore vel tenetur inventore in architecto? Placeat sit doloremque reprehenderit alias animi aut, qui sed quia!</p>
+<p>[b]Lorem ipsum[/b], dolor sit amet consectetur adipisicing elit. Ipsa distinctio eligendi laudantium praesentium est tempore vel tenetur inventore in architecto? Placeat sit doloremque reprehenderit alias animi aut, qui sed quia!</p>
